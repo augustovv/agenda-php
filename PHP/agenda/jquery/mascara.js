a9f0a5d1285636afa8a1ec9c6435568ad570628c
Augusto <101065765+augustovv@users.noreply.github.com>
2024-04-22 12:48:25 -0300

    agenda-php

---
+++ PHP/agenda/jquery/mascara.js
@@ -0,0 +1,9 @@
+  $(document).ready(function(){
+            $('#cpf').mask('000.000.000-00');
+            $('#cep').mask('00000-000');
+        })
+
+
+var nome = "AAA" 
+var nome2 = JSON.stringify(nome);
+localStorage.setItem("string",nome2);
